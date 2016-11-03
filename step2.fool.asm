61602a9d1820808be4bbffd588b925cb951a95fc
Alessandro Contro <alessandro.contro@gmail.com>
2016-11-03 18:52:03 +0100

    Added some tests

---
+++ step2.fool.asm
@@ -0,0 +1,56 @@
+push 0
+push function0
+push function1
+push -2
+lfp
+add
+lw
+push -2
+lfp
+add
+print
+halt
+
+function0:
+cfp
+lra
+push 1
+lfp
+add
+lw
+push 2
+lfp
+add
+lw
+add
+srv
+sra
+sfp
+pop
+pop
+pop
+lrv
+lra
+js
+
+function1:
+cfp
+lra
+push 1
+lfp
+add
+lw
+push 2
+lfp
+add
+lw
+sub
+srv
+sra
+sfp
+pop
+pop
+pop
+lrv
+lra
+js
